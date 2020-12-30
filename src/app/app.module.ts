a29ddac56795367505b597277046c3b1285c9caa
Guilherme de Andrade Moura <guileon64@gmail.com>
2020-12-30 10:12:59 -0300

    fonts and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
